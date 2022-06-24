28dea45cba50064821093a6ba5eff25e1927fe24
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-06-24 12:49:29 +0530

    Add solution to repeatString problem of Foundation

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    // Declare a variable & assign an empty string to it, unassigned vars are undefined by default
+    // If the num is 0, our function will return undefined but the test expects an empty string
+    let result = "";
 
+    for (let i = 0; i < num; i++) result += string;
+
+    // If num is a negative no, return an error. You can't repeat something negative times, can you?
+    return (num < 0) ? "ERROR" : result;
 };
 
 // Do not edit below this line
