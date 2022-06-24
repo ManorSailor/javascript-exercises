2b6d595e8320e303641b53a7cc92e0b01a58641d
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-06-24 15:41:03 +0530

    Add solution to reverseString problem of Foundations

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,24 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let length = string.length;
+    let reversedString = "";
 
+    // Strings are just an array of characters, you can access individual characters of a string using [] notation
+    // Starting from length - 1 because it is the same last element of array. Array index starts from 0.
+    // We start accessing elements from the end of string & append it to a new variable
+    for (let i = length - 1; i >= 0; i--) reversedString += string[i];
+
+    return reversedString;
 };
+
+// Alternate method doing the same thing
+// const reverseString = function(string) {
+//     let length = string.length;
+//     let reversedString = "";
+
+//     for (let i = 0; i < length; i++) reversedString += string[length - (i + 1)];
+
+//     return reversedString;
+// };
 
 // Do not edit below this line
 module.exports = reverseString;
