7626d953f0a2757dd6bf53578be7e399134988dc
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-06-24 16:59:24 +0530

    Add solution to removeFromArray problem of Foundation

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+// Get the array & argument to remove
+// Check if the list contains said element via loop
+// If it does, remove it
+// return the array
 
+const removeFromArray = function(list, ...element) {
+    for (const item of element) {
+        if (list.includes(item)) list.splice(list.indexOf(item), 1);
+    }
+
+    return list;
 };
 
 // Do not edit below this line
