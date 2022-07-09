89e54a5f0a9648596c43091340a4c1ffe6cf3c1c
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-07-09 21:59:51 +0530

    Add solution to palindromes problem of Foundations

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (original) {
+    original = original.toLowerCase().replace(/[^a-z]/g, '');
+    reversed = reverseString(original).toLowerCase().replace(/[^a-z]/g, '');
+    return (original === reversed) ? true : false;
 };
+
+const reverseString = function(string) {
+    let length = string.length;
+    let reversedString = "";
+
+    for (let i = length - 1; i >= 0; i--) reversedString += string[i];
+
+    return reversedString;
+};
+
 
 // Do not edit below this line
 module.exports = palindromes;
