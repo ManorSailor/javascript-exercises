8c3d480c5846c80aba7fa05b4d833782c6faed78
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-07-09 18:12:00 +0530

    Add solution to calculator problem of Foundations

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(...numbers) {
+	return numbers.reduce((sum, num) => sum + num, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(...numbers) {
+  let result = 0;
+  for (let i = 0; i < numbers.length - 1; i++) {
+    result = numbers[i] - numbers[i + 1];
+  }
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((sum, num) => sum * num, 1);
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  return number ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	return (number === 0 || number === 1) ? 1 : factorial(number - 1) * number;
 };
 
 // Do not edit below this line
