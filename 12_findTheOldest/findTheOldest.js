aa07a5452ca4151e1251e132f813e618d30b7d7c
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-07-10 17:33:48 +0530

    Add solution to findTheOldest problem of Foundations

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((personA, personB) => {
+        const aAge = (!personA.yearOfDeath) ? new Date().getFullYear() - personA.yearOfBirth : personA.yearOfDeath - personA.yearOfBirth;
+        const bAge = (!personB.yearOfDeath) ? new Date().getFullYear() - personB.yearOfBirth : personB.yearOfDeath - personB.yearOfBirth;
+        return (aAge > bAge) ? personA : personB;
+    });
 };
 
 // Do not edit below this line
